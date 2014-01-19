2ac82266479d5528a2c85fed4f6a0e30007580e3
markotaht <markotaht@gmail.com>
2014-01-19 10:32:56 +0200

    Tsting

---
+++ src/controller/Controller.java
@@ -0,0 +1,266 @@
+package controller;
+
+import java.util.Random;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.Sys;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.util.vector.Vector3f;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.util.glu.GLU.gluPerspective;
+
+public class Controller {
+	
+	private static Vector3f position = new Vector3f(0,0,0);
+	private static Vector3f rotation = new Vector3f(0,0,0);
+	private static int walkingSpeed = 10;
+	private static int mouseSpeed = 1;
+	private static long lastFrame;
+	private static final int maxLookUp = 85;
+	private static final int maxLookDown = -85;
+	
+	private static long getTime(){
+		return(Sys.getTime()* 1000) / Sys.getTimerResolution();
+	}
+	
+	private static int getDelta(){
+		long currentTime = getTime();
+		int delta = (int)(currentTime - lastFrame);
+		lastFrame = getTime();
+		return delta;
+	}
+	public Controller(){
+		try{
+			Display.setDisplayMode(new DisplayMode(640,480));
+			Display.setTitle("Controller");
+			Display.create();
+		} catch(LWJGLException e){
+			e.printStackTrace();
+		}
+
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		gluPerspective((float) 30, 640f / 480f, 0.001f, 100);
+		glMatrixMode(GL_MODELVIEW);
+		
+		Point[] points = new Point[10000];
+		Random random = new Random();
+		
+		for(int i = 0; i < points.length; i++){
+			points[i] = new Point((random.nextFloat() - 0.5f)* 100f,
+								  (random.nextFloat() - 0.5f)* 100f,
+								  random.nextInt(200) - 200);
+		}
+		
+		
+		while(!Display.isCloseRequested()){
+			int delta = getDelta();
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+			glRotatef(rotation.x,1,0,0);
+			glRotatef(rotation.y,0,1,0);
+			glRotatef(rotation.z,0,0,1);
+			glTranslatef(position.x,position.y,position.z);
+			
+			glBegin(GL_POINTS);
+			for(Point p: points){
+				glVertex3f(p.x,p.y,p.z);
+			}
+			glEnd();
+			
+            if (Mouse.isGrabbed()) {
+                float mouseDX = Mouse.getDX() * mouseSpeed * 0.0016f;
+                float mouseDY = Mouse.getDY() * mouseSpeed * 0.0016f;
+                if (rotation.y + mouseDX >= 360) {
+                    rotation.y = rotation.y + mouseDX - 360;
+                } else if (rotation.y + mouseDX < 0) {
+                    rotation.y = 360 - rotation.y + mouseDX;
+                } else {
+                    rotation.y += mouseDX;
+                }
+                if (rotation.x - mouseDY >= maxLookDown && rotation.x - mouseDY <= maxLookUp) {
+                    rotation.x += -mouseDY;
+                } else if (rotation.x - mouseDY < maxLookDown) {
+                    rotation.x = maxLookDown;
+                } else if (rotation.x - mouseDY > maxLookUp) {
+                    rotation.x = maxLookUp;
+                }
+            }
+			
+			boolean keyUp = Keyboard.isKeyDown(Keyboard.KEY_UP) || Keyboard.isKeyDown(Keyboard.KEY_W);
+			boolean keyDown = Keyboard.isKeyDown(Keyboard.KEY_DOWN) || Keyboard.isKeyDown(Keyboard.KEY_S);
+			boolean keyLeft = Keyboard.isKeyDown(Keyboard.KEY_LEFT) || Keyboard.isKeyDown(Keyboard.KEY_A);
+			boolean keyRight = Keyboard.isKeyDown(Keyboard.KEY_RIGHT) || Keyboard.isKeyDown(Keyboard.KEY_D);
+			boolean flyUp = Keyboard.isKeyDown(Keyboard.KEY_SPACE);
+            boolean flyDown = Keyboard.isKeyDown(Keyboard.KEY_LSHIFT);
+            boolean moveFaster = Keyboard.isKeyDown(Keyboard.KEY_LCONTROL);
+            boolean moveSlower = Keyboard.isKeyDown(Keyboard.KEY_TAB);
+            
+            if (moveFaster && !moveSlower) {
+                walkingSpeed *= 4f;
+            }
+            if (moveSlower && !moveFaster) {
+                walkingSpeed /= 10f;
+            }
+
+            if (keyUp && keyRight && !keyLeft && !keyDown) {
+                float angle = rotation.y + 45;
+                Vector3f newPosition = new Vector3f(position);
+                float hypotenuse = (walkingSpeed * 0.0002f) * delta;
+                float adjacent = hypotenuse * (float) Math.cos(Math.toRadians(angle));
+                float opposite = (float) (Math.sin(Math.toRadians(angle)) * hypotenuse);
+                newPosition.z += adjacent;
+                newPosition.x -= opposite;
+                position.z = newPosition.z;
+                position.x = newPosition.x;
+            }
+            if (keyUp && keyLeft && !keyRight && !keyDown) {
+                float angle = rotation.y - 45;
+                Vector3f newPosition = new Vector3f(position);
+                float hypotenuse = (walkingSpeed * 0.0002f) * delta;
+                float adjacent = hypotenuse * (float) Math.cos(Math.toRadians(angle));
+                float opposite = (float) (Math.sin(Math.toRadians(angle)) * hypotenuse);
+                newPosition.z += adjacent;
+                newPosition.x -= opposite;
+                position.z = newPosition.z;
+                position.x = newPosition.x;
+            }
+            if (keyUp && !keyLeft && !keyRight && !keyDown) {
+                float angle = rotation.y;
+                Vector3f newPosition = new Vector3f(position);
+                float hypotenuse = (walkingSpeed * 0.0002f) * delta;
+                float adjacent = hypotenuse * (float) Math.cos(Math.toRadians(angle));
+                float opposite = (float) (Math.sin(Math.toRadians(angle)) * hypotenuse);
+                newPosition.z += adjacent;
+                newPosition.x -= opposite;
+                position.z = newPosition.z;
+                position.x = newPosition.x;
+            }
+            if (keyDown && keyLeft && !keyRight && !keyUp) {
+                float angle = rotation.y - 135;
+                Vector3f newPosition = new Vector3f(position);
+                float hypotenuse = (walkingSpeed * 0.0002f) * delta;
+                float adjacent = hypotenuse * (float) Math.cos(Math.toRadians(angle));
+                float opposite = (float) (Math.sin(Math.toRadians(angle)) * hypotenuse);
+                newPosition.z += adjacent;
+                newPosition.x -= opposite;
+                position.z = newPosition.z;
+                position.x = newPosition.x;
+            }
+            if (keyDown && keyRight && !keyLeft && !keyUp) {
+                float angle = rotation.y + 135;
+                Vector3f newPosition = new Vector3f(position);
+                float hypotenuse = (walkingSpeed * 0.0002f) * delta;
+                float adjacent = hypotenuse * (float) Math.cos(Math.toRadians(angle));
+                float opposite = (float) (Math.sin(Math.toRadians(angle)) * hypotenuse);
+                newPosition.z += adjacent;
+                newPosition.x -= opposite;
+                position.z = newPosition.z;
+                position.x = newPosition.x;
+            }
+            if (keyDown && !keyUp && !keyLeft && !keyRight) {
+                float angle = rotation.y;
+                Vector3f newPosition = new Vector3f(position);
+                float hypotenuse = -(walkingSpeed * 0.0002f) * delta;
+                float adjacent = hypotenuse * (float) Math.cos(Math.toRadians(angle));
+                float opposite = (float) (Math.sin(Math.toRadians(angle)) * hypotenuse);
+                newPosition.z += adjacent;
+                newPosition.x -= opposite;
+                position.z = newPosition.z;
+                position.x = newPosition.x;
+            }
+            if (keyLeft && !keyRight && !keyUp && !keyDown) {
+                float angle = rotation.y - 90;
+                Vector3f newPosition = new Vector3f(position);
+                float hypotenuse = (walkingSpeed * 0.0002f) * delta;
+                float adjacent = hypotenuse * (float) Math.cos(Math.toRadians(angle));
+                float opposite = (float) (Math.sin(Math.toRadians(angle)) * hypotenuse);
+                newPosition.z += adjacent;
+                newPosition.x -= opposite;
+                position.z = newPosition.z;
+                position.x = newPosition.x;
+            }
+            if (keyRight && !keyLeft && !keyUp && !keyDown) {
+                float angle = rotation.y + 90;
+                Vector3f newPosition = new Vector3f(position);
+                float hypotenuse = (walkingSpeed * 0.0002f) * delta;
+                float adjacent = hypotenuse * (float) Math.cos(Math.toRadians(angle));
+                float opposite = (float) (Math.sin(Math.toRadians(angle)) * hypotenuse);
+                newPosition.z += adjacent;
+                newPosition.x -= opposite;
+                position.z = newPosition.z;
+                position.x = newPosition.x;
+            }
+            if (flyUp && !flyDown) {
+                double newPositionY = (walkingSpeed * 0.0002) * delta;
+                position.y -= newPositionY;
+            }
+            if (flyDown && !flyUp) {
+                double newPositionY = (walkingSpeed * 0.0002) * delta;
+                position.y += newPositionY;
+            }
+            if (moveFaster && !moveSlower) {
+                walkingSpeed /= 4f;
+            }
+            if (moveSlower && !moveFaster) {
+                walkingSpeed *= 10f;
+            }
+            while (Mouse.next()) {
+                if (Mouse.isButtonDown(0)) {
+                    Mouse.setGrabbed(true);
+                }
+                if (Mouse.isButtonDown(1)) {
+                    Mouse.setGrabbed(false);
+                }
+            }
+            while (Keyboard.next()) {
+                if (Keyboard.isKeyDown(Keyboard.KEY_C)) {
+                    position = new Vector3f(0, 0, 0);
+                    rotation = new Vector3f(0, 0, 0);
+                }
+                if (Keyboard.isKeyDown(Keyboard.KEY_O)) {
+                    mouseSpeed += 1;
+                    System.out.println("Mouse speed changed to " + mouseSpeed + ".");
+                }
+                if (Keyboard.isKeyDown(Keyboard.KEY_L)) {
+                    if (mouseSpeed - 1 > 0) {
+                        mouseSpeed -= 1;
+                        System.out.println("Mouse speed changed to " + mouseSpeed + ".");
+                    }
+                }
+                if (Keyboard.isKeyDown(Keyboard.KEY_Q)) {
+                    System.out.println("Walking speed changed to " + walkingSpeed + ".");
+                    walkingSpeed += 1;
+                }
+                if (Keyboard.isKeyDown(Keyboard.KEY_Z)) {
+                    System.out.println("Walking speed changed to " + walkingSpeed + ".");
+                    walkingSpeed -= 1;
+                }
+            }
+			
+			
+			Display.update();
+			Display.sync(60);			
+		}
+		Display.destroy();
+		System.exit(0);
+	}
+	
+	private static class Point{
+		float x, y, z;
+		
+		public Point(float x, float y, float z){
+			this.x = x;
+			this.y = y;
+			this.z = z;
+		}
+	}
+	
+	public static void main(String[] args){
+		new Controller();
+	}
+	
+}
